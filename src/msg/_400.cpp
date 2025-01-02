94a75d4a76133ff14c6b9046dcb1431a471e1158
Riccardo Leone <riccardo.leone97@gmail.com>
2025-01-03 00:36:03 +0100

    [delete files]

--- src/msg/_400.cpp
+++
@@ -1,6 +0,0 @@
-#include "_400.hpp"
-
-_400::_400(): Response(400, "Bad Request"){}
-
-_400::~_400(){}
-
--- src/msg/_500.cpp
+++
@@ -1,6 +0,0 @@
-#include "_500.hpp"
-
-_500::_500(): Response(400, "Internal Server Error"){}
-
-_500::~_500(){}
-
