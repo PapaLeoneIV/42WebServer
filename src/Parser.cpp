2e8a07c6d5b96b2ac6c016731ceac415f1a06e07
PapaLeoneIV <riccardo.leone97@gmail.com>
2025-01-13 11:30:04 +0100

    [Parser Class] improved header parsing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -32,6 +32,67 @@ Parser::Parser() {
 
 Parser::~Parser(){}
 
+
+void Parser::validateResource(Client *client, Server *server)
+{
+    Request *request = client->getRequest();
+    Response *response = client->getResponse();
+
+    std::string fullPath = (std::string(getcwd(NULL, 0)) +  server->getRoot() + request->getUrl());
+    struct stat sb;
+
+    if(access(fullPath.c_str(), F_OK) == -1){
+        response->setStatusCode(404);
+        response->setStatusMessage("Not Found");
+        return ;
+    }
+
+    if(stat(fullPath.c_str(), &sb) == -1){
+        response->setStatusCode(500);
+        response->setStatusMessage("Internal Server Error");
+        return ;
+    }
+
+
+    if(S_ISREG(sb.st_mode)){
+        if(checkPermissions(fullPath, R_OK) != SUCCESS){
+            response->setStatusCode(403);
+            response->setStatusMessage("Forbidden: Permission Denied: Read access denied");
+            return ;
+        }
+    }
+    else if(S_ISDIR(sb.st_mode)){
+        if(checkPermissions(fullPath, R_OK) == SUCCESS){
+            response->setStatusCode(403);
+            response->setStatusMessage("Forbidden: Permission Denied: Read access denied");
+            return ;
+        }
+        //TODO check if index.html exists
+        //TODO check if the directory is readable
+        //TODO check if the directory is executable
+    }else{
+        response->setStatusCode(403);
+        response->setStatusMessage("Forbidden: Permission Denied: Not a regular file or directory");
+        return ;
+    }
+    
+
+    //check if is it accessible
+    if(access(fullPath.c_str(), F_OK) == -1){
+        response->setStatusCode(404);
+        response->setStatusMessage("Not Found");
+        return ;
+    }
+    //check if is it readable
+    if(access(fullPath.c_str(), R_OK) == -1){
+        response->setStatusCode(403);
+        response->setStatusMessage("Forbidden: Permission Denied: Read access denied");
+        return ;
+    }
+
+    return ;
+}
+
 void Parser::parse(Request *request, Client *client) {
 
     std::string dataStr(client->getRequestData());
@@ -69,7 +130,8 @@ void Parser::parse(Request *request, Client *client) {
 
     //TODO check section 3.2 of RFC 2616 for the correct format of the URL
     request->print();
-    std::string url = request->getUrl();
+    std::string url = analyzeUrl(request->getUrl());
+
     if (url.find_first_not_of(ALLOWED_CHARS) != std::string::npos || url.find_first_of("/") != 0) {
         client->getResponse()->setStatusCode(400);
         client->getResponse()->setStatusMessage("Bad Request: Invalid URL");
@@ -90,11 +152,9 @@ void Parser::parse(Request *request, Client *client) {
         if (request->getBody().size() != static_cast<size_t>(strToInt(contentLen))) {
             client->getResponse()->setStatusCode(400);
             client->getResponse()->setStatusMessage("Bad Request: Content-Length does not match the body size");
-
             return;
         }
-        }
-        else if(request->getHeaders().find("transfer-encoding") != request->getHeaders().end()) {
+        } else if(request->getHeaders().find("transfer-encoding") != request->getHeaders().end()) {
             if (request->getHeaders().find("transfer-encoding")->second == "chunked") {
                 //TODO handle it in the future
             }
@@ -104,7 +164,6 @@ void Parser::parse(Request *request, Client *client) {
             client->getResponse()->setStatusMessage("Length Required");
             return;
         }
-        
     }
 }
 
@@ -130,17 +189,25 @@ Request* Parser::decompose(char *data) {
 
     std::map<std::string, std::string> headers;
 
-    while (std::getline(iss, line) && line[0] != '\r' && line[1] != '\n') {   
+
+    //TODO update check on CLRF
+    while (std::getline(iss, line) && line != "\r\n" && !line.empty()) {   
         size_t colonPos = line.find(':');
+
         if (colonPos != std::string::npos) {
             std::string headerName = line.substr(0, colonPos);
             std::string headerValue = line.substr(colonPos + 1);
 
-            headerName = headerName.substr(headerName.find_first_not_of(" \t"));
-            headerValue = headerValue.substr(headerValue.find_first_not_of(" \t"));
+            // Remove all carriage return, space, tabs from key and value
+            headerName.erase(std::remove(headerName.begin(), headerName.end(), '\r'), headerName.end());
+            headerName.erase(std::remove(headerName.begin(), headerName.end(), ' '), headerName.end());
+            headerName.erase(std::remove(headerName.begin(), headerName.end(), '\t'), headerName.end());
+            headerValue.erase(std::remove(headerValue.begin(), headerValue.end(), '\r'), headerValue.end());
+            headerValue.erase(std::remove(headerValue.begin(), headerValue.end(), ' '), headerValue.end());
+            headerValue.erase(std::remove(headerValue.begin(), headerValue.end(), '\t'), headerValue.end());
 
-            //case insensitive
-            headerName = to_lowercase(headerName);
+            // Case insensitive
+            std::transform(headerName.begin(), headerName.end(), headerName.begin(), ::tolower);
 
             headers[headerName] = headerValue;
         }
