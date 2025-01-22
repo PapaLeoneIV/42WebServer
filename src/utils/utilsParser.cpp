50219ad0dc2eb130acf102855e97baef080974af
PapaLeoneIV <riccardo.leone97@gmail.com>
2025-01-22 12:13:56 +0100

    [TODO Issue test]

--- src/utils/utilsParser.cpp
+++ src/utils/utilsParser.cpp
@@ -128,7 +128,7 @@ ERROR Parser::extractBody(Request *request, std::istringstream &bodyStream) {
     }
     return SUCCESS;
 }
-
+//TODO TEST
 void Parser::parseMultipart(Request *request, std::istringstream &formDataStream, std::string boundary) {
 
     std::string sections = extractBodyFromStream(formDataStream, boundary);
