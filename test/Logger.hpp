252e5fec2facf1cecebeb9e3c3b4424b9a56d514
PapaLeoneIV <riccardo.leone97@gmail.com>
2025-02-16 18:14:37 +0100

    [Logger] Added Logger Class to print errors and info

---
+++ test/Logger.hpp
@@ -0,0 +1,16 @@
+#ifndef LOGGER
+#define LOGGER
+
+#include <iostream>
+
+
+class Logger {
+    private:
+        Logger();
+    
+    public:
+        static void info(const std::string& message);
+    
+        static void error(const std::string& file, const std::string& message);
+    };
+#endif //LOGGER
