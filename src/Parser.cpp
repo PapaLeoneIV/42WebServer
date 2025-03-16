9f6acd0d6fe199a571bee68e47ebc497f986972b
PapaLeoneIV <PapaLeoneIV@drip.com>
2025-03-16 19:22:18 +0100

    [matching string function for locationConfig]

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -49,6 +49,52 @@
     
 //     return request;
 // }
+unsigned int Parser::levenshtein_distance(const std::string& s1, const std::string& s2)
+{
+	const std::size_t len1 = s1.size(), len2 = s2.size();
+	std::vector<std::vector<unsigned int>> d(len1 + 1, std::vector<unsigned int>(len2 + 1));
+
+	d[0][0] = 0;
+	for(unsigned int i = 1; i <= len1; ++i) d[i][0] = i;
+	for(unsigned int i = 1; i <= len2; ++i) d[0][i] = i;
+
+	for(unsigned int i = 1; i <= len1; ++i)
+		for(unsigned int j = 1; j <= len2; ++j)
+                      d[i][j] = std::min(std::min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));  
+	return d[len1][len2];
+}
+
+std::string Parser::findBestApproximationString(std::string url, std::vector<std::string> dictionary) {
+
+    int min_distance = INT_MAX;
+    std::string best_match;
+    size_t i;
+
+    for (i = 0; i < dictionary.size(); i++) {
+        int distance = levenshtein_distance(url, dictionary[i]);
+        if (distance < min_distance) {
+            min_distance = distance;
+            best_match = dictionary[i];
+        }
+    }
+
+    return best_match;
+}
+
+
+std::map<std::string, std::vector<std::string> >  Parser::getMatchingLocation(std::string url, Server *server){
+    std::vector<std::string> dictionary;
+
+
+    std::map<std::string, std::map<std::string, std::vector<std::string> > >::iterator it = server->getLocationDir().begin();
+    for(; it != server->getLocationDir().end(); it++){
+        dictionary.push_back(it->first);
+    }
+
+
+    return server->getLocationDir()[findBestApproximationString(url, dictionary)];
+
+}
 
 void Parser::validateResource(Client *client, Server *server)
 {
@@ -61,7 +107,9 @@ void Parser::validateResource(Client *client, Server *server)
 
     if(!request || !response)
         return;
-
+    
+    
+   // std::map<std::string, std::vector<std::string> >  LocationConfig = getMatchingLocation(client->getRequest()->getUrl(), client->getServer());
     // TODO: atm is hardcoded to the root directory
     // Issue URL: https://github.com/PapaLeoneIV/42WebServer/issues/6
     std::string filePath = server->getCwd() +  server->getRoot() + request->getUrl();
